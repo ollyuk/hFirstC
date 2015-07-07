3e4457376d48ccb72a175303b7aa6d8974905553
Oliver Cumming <cumming.oliver@gmail.com>
2015-07-07 08:11:33 +0100

    chapter1

--- cards.c
+++ cards.c
@@ -7,27 +7,43 @@ int main()
 	puts("Enter the card_name: ");
 	scanf("%2s", card_name);
 	int val = 0;
-	
-	switch(card_name[ 0]) {
-		case 'K':
-		case 'Q':
-		case 'J':
-			val = 10;
-			break;
-		case 'A':
-			val = 11;
-			break;
-		default:
-			val = atoi(card_name);
+	int count = 0;
+
+	while (card_name[ 0] != 'X'){
+		puts("Enter the card_name: ");
+		scanf("%2s", card_name);
+		switch(card_name[ 0]) {
+			case 'K':
+			case 'Q':
+			case 'J':
+				val = 10;
+				break;
+			case 'A':
+				val = 11;
+				break;
+			case 'X':
+				continue; //takes us back to the start of the while loop - then leaves the loop as card_name[0] = X
+			default:
+				val = atoi(card_name);
+				if ((val < 1) || (val > 10)) {
+					puts("Card out of range (1-10).");
+					continue;
+				} 
+		}
+			printf("The card value is: %i\n", val);
+
+			/* Check to see if count is going up or down */
+
+			if ((val > 2) && (val < 7)) {
+				puts("Count has gone up");
+				count++;
+			}
+			if (val == 10){
+				puts("Count has gone down");
+				count--; 
+			}
+			printf("Current count is: %i\n", count);
+		
 	}
-
-	printf("The card value is: %i\n", val);
-
-	/* Check to see if count is going up or down */
-
-	if ((val > 2) && (val < 7)) 
-		puts("Count has gone up");
-	else if (val == 10)
-		puts("Count has gone down"); 
 	return 0;
 }
